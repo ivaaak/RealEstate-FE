ce92fff98b310fe80cc0e73c8bf02c250f2184ad
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-03-21 23:30:50 +0200

    feat: re-generate project structure

---
+++ src/app/app-root/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { StayService } from '../services/stay.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(private stayService: StayService,
+    private translate: TranslateService) {
+    translate.setDefaultLang('en')
+    translate.use('en')
+  }
+
+  title = 'realestate';
+}
