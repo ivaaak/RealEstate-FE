451edaa38c60c4d5980de3098a58b70fdd9a5100
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-04-18 17:46:13 +0300

    feat: special perks comp, guest, stay edit

---
+++ src/app/cmps/details/stay-order/stay-order.component.ts
@@ -0,0 +1,116 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { StatReviews, Stay } from 'src/app/models/stay.model';
+import { faStar, faCircleMinus, faCirclePlus, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { CalendarOptions } from 'ngx-airbnb-calendar';
+import { Order } from 'src/app/models/order.model';
+import { UtilService } from 'src/app/services/util.service';
+
+@Component({
+  selector: 'stay-order',
+  templateUrl: './stay-order.component.html',
+  styleUrls: ['./stay-order.component.scss']
+})
+
+
+export class StayOrderComponent implements OnInit {
+  constructor(private utilService: UtilService) { }
+
+  @Input() stay !: Stay
+  @Input() order !: Order
+  @Output() addOrder = new EventEmitter()
+
+  faCirclePlus = faCirclePlus
+  faCircleMinus = faCircleMinus
+  faChevronDown = faChevronDown
+  faChevronUp = faChevronUp
+  faStar = faStar
+  date: string | null = null
+  totalDays!: any
+  children: number = 0
+  showGuestModal: boolean = false
+
+  options: CalendarOptions = {
+    format: "yyyy/LL/dd",
+    formatDays: "eeeeee",
+    firstCalendarDay: 1,
+    closeOnSelected: true,
+  }
+
+  ngOnInit () {
+    if (!this.order.startDate.getTime()) this.order.startDate = new Date()
+    if (!this.order.endDate.getTime()) this.order.endDate = new Date(Date.now() + (3600 * 1000 * 72))
+    this.date = this.dateFromOrder
+  }
+
+  onAddOrder () {
+    this.addOrder.emit()
+  }
+
+  public toggleGuestModal () {
+    this.showGuestModal = !this.showGuestModal
+  }
+
+  get GetTotalDays (): number {
+    return this.utilService.getDaysBetweenDates(this.getCheckOut(), this.getCheckIn())
+  }
+
+  get Price () {
+    return this.stay.price * this.GetTotalDays
+  }
+
+  get CleanTax () {
+    return (this.Price * 0.10).toFixed()
+  }
+
+  get ServiceFee () {
+    return (this.Price * 0.17).toFixed()
+  }
+
+  get TotalPrice () {
+    return (+this.Price + +this.CleanTax + +this.ServiceFee)
+  }
+
+  get dateFromOrder () {
+    if (!this.order.startDate.getMilliseconds() || !this.order.endDate.getMilliseconds()) return ''
+    return this.order.startDate.toDateString() + '-' + this.order.endDate.toDateString()
+  }
+
+  get RateAvg () {
+    let rate = 0
+    let key: keyof StatReviews
+    for (key in this.stay.statReviews) {
+      rate += this.stay.statReviews[key]
+    }
+
+    return (rate / 6).toFixed(2)
+  }
+
+  getGuests () {
+    let str = this.order.guests.adults + this.order.guests.children > 0 ? (this.order.guests.adults + this.order.guests.children) + ' guests ' : ''
+    str += this.order.guests.infants > 0 ? ' ,' + this.order.guests.infants + ' infants ' : ''
+    str += this.order.guests.pets > 0 ? ' ,' + this.order.guests.pets + ' pets ' : ''
+    return str
+  }
+
+  getCheckIn () {
+    if (this.date) {
+      const dates = this.date?.split('-')
+      if (dates.length >= 1) {
+        this.order.startDate = new Date(dates[0])
+        return new Date(dates[0])
+      }
+    }
+    return this.order.startDate
+  }
+
+  getCheckOut () {
+    if (this.date) {
+      const dates = this.date?.split('-')
+      if (dates.length === 2) {
+        this.order.endDate = new Date(dates[1])
+        return new Date(dates[1])
+      }
+    }
+    return this.order.endDate
+  }
+}
---
+++ src/app/cmps/edit-stay/edit-stay.component.ts
@@ -0,0 +1,157 @@
+import { Component, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { StayService } from 'src/app/services/stay.service';
+import { UploadImgService } from 'src/app/services/upload-img.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'edit-stay',
+  templateUrl: './edit-stay.component.html',
+  styleUrls: ['./edit-stay.component.scss']
+})
+export class EditStayComponent {
+  constructor(private stayService: StayService,
+    private uploadImgService: UploadImgService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar) { }
+
+  @Input() user!: User
+  stay = this.stayService.getEmptyStay()
+  imgData = new Array(5).fill({ imgUrl: '', height: 500, width: 500 })
+  selectSettings = {}
+  amenities = this.Amenities
+  labels = this.Labels
+
+  async ngOnInit () {
+    this.selectSettings = this.Settings
+    const stayId = this.route.snapshot.paramMap.get('id')
+    if (stayId) this.loadStay(stayId)
+  }
+
+  async loadStay (stayId: string) {
+    try {
+      this.stay = await lastValueFrom(this.stayService.getById(stayId)) as any
+      this.loadImg()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  loadImg (): void {
+    this.imgData = this.imgData.map((_, idx) => {
+      return { imgUrl: this.stay.imgUrls?.[idx], height: 500, width: 500 }
+    })
+  }
+
+  async onAddStay () {
+    const user = this.userService.getUser()
+    const country = this.stay.loc.country
+    const city = this.stay.loc.city
+    const address = this.stay.loc.address
+    this.stay.loc.address = address
+    this.stay.host = { ...this.stay.host, _id: user._id, pictureUrl: user.imgUrl, fullname: user.fullname }
+    if (!this.checkValidation()) return
+    this.stay.loc.address = `${address}, ${city}, ${country}`
+    try {
+      await this.stayService.save(this.stay)
+      this.snackBar.open('Stay saved successfully', 'Close', { duration: 3000 })
+      this.router.navigate(['/user/stays'])
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  checkValidation () {
+    const stay = this.stay
+    if (stay.imgUrls.length < 5) {
+      this.snackBar.open('You must add 5 images', 'Close', { duration: 3000 })
+      return false
+    }
+    if (stay.capacity < 1) {
+      this.snackBar.open('You must add at least 1 capacity', 'Close', { duration: 3000 })
+      return false
+    }
+    if (stay.name === '') {
+      this.snackBar.open('You must add stay name', 'Close', { duration: 3000 })
+      return false
+    }
+    if (stay.price < 1) {
+      this.snackBar.open('You must add valid price', 'Close', { duration: 3000 })
+      return false
+    }
+    if (!/^[a-zA-Z]{3,}$/.test(stay.loc.country)) {
+      this.snackBar.open('You must add valid country', 'Close', { duration: 3000 })
+      return false
+    }
+    if (!/^[a-zA-Z]{2,}$/.test(stay.loc.city)) {
+      this.snackBar.open('You must add valid city', 'Close', { duration: 3000 })
+      return false
+    }
+    if (!/^[a-zA-Z][a-zA-Z0-9\s]{2,}$/.test(stay.loc.address)) {
+      this.snackBar.open('You must add valid address', 'Close', { duration: 3000 })
+      return false
+    }
+    return true
+  }
+
+  async uploadImg (ev: Event, index: number) {
+    const { secure_url, height, width } = await this.uploadImgService.uploadImg(ev)
+    this.imgData[index] = { imgUrl: secure_url, width, height }
+    const imgUrl = this.imgData[index].imgUrl
+    this.stay.imgUrls.push(imgUrl)
+  }
+
+  get Labels () {
+    return [
+      'OMG!',
+      'Amazing views', ,
+      'Trending',
+      'Golfing',
+      'Surfing',
+      'Mansions',
+      'Luxe',
+      'Private rooms', ,
+      'Lakefront', ,
+      'Castles',
+      'Tiny homes', ,
+      'Islands',
+      'Boats',
+      'Creative spaces', ,
+      'Beach',
+      'Design',
+    ]
+  }
+
+  get Amenities () {
+    return [
+      '32" HDTV with Disney+, standard cable',
+      "Wifi",
+      "AC - split type ductless system",
+      "Private outdoor pool - available all year, open 24 hours, lap pool",
+      "Kitchen",
+      "Free parking on premises",
+      "Hot water",
+      "Heating - split type ductless system",
+      "Indoor fireplace: wood-burning",
+      "Shampoo",
+      "Hair dryer",
+      "Hot water",
+      "Backyard",
+      "Pets allowed"
+    ]
+  }
+  get Settings () {
+    return {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All'
+    }
+  }
+}
